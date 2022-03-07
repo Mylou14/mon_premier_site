37a9220342bb6cec8852315dc9722770a8c73b55
Mylou14 <101122244+Mylou14@users.noreply.github.com>
2022-03-07 16:14:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+
+setTimeout(function() {
 var result = prompt("Quel est ton nom ?");
 var element = document.getElementById('nom');
 if(result != null){
   element.innerHTML = "Merci d'avoir visité mon site, " + result + " !";
 }
+},2000);
